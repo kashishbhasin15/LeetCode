c6b7a8fc2a569848997a2d5b3f9823af537ed609
kashishbhasin15 <159510536+kashishbhasin15@users.noreply.github.com>
2026-09-11 17:16:50 +0530

    Time: 0 ms (100%), Space: 9.1 MB (82.03%) - LeetHub

---
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.c
@@ -0,0 +1,19 @@
+char nextGreatestLetter(char* letters, int lettersSize, char target) {
+    
+int low=0;
+int high=lettersSize-1;
+
+while(low<=high)
+{
+    int mid=(low+high)/2;
+    if(letters[mid]<=target)
+    {
+        low=mid+1;
+    }
+    else
+    {
+        high=mid-1;
+    }
+}
+return letters[low%lettersSize];
+}
