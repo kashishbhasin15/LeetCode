559e9987cd3902db2e805899bd9decbdc7fa209b
kashishbhasin15 <159510536+kashishbhasin15@users.noreply.github.com>
2026-09-13 20:24:23 +0530

    Time: 327 ms (96.67%), Space: 0B (100%) - LeetHub

---
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+select name from employee where id in (
+    select managerId from employee where managerId is not NULL group by managerId having count(*)>=5
+);
